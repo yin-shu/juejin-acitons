a95b4ae6d9f019f9109d078ba1a16125acffb22f
yinshu <yinshu@rongcloud.cn>
2022-01-30 11:31:49 +0800

    chore: update

--- src/utils/wxPush.js
+++ src/utils/wxPush.js
@@ -5,7 +5,7 @@ const PUSH_KEY = 'SCT116767TIeEM1TmkBuQrdkRphNcHsB9O'
 const wxPush = async (title = '1', desp = '') => {
   const options = {
     hostname: 'sctapi.ftqq.com',
-    path: `/${PUSH_KEY}.send?title=${encodeURIComponent(title)}&desp=${encodeURIComponent(desp)}`
+    path: `/${process.env.FANG_TANG_SEND_KEY}.send?title=${encodeURIComponent(title)}&desp=${encodeURIComponent(desp)}`
   }
   console.log(options)
   return await request(options)
